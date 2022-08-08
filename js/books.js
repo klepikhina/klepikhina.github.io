835eeea3577a50357e69fa155d43a7dd88d5f354
Ksenia Lepikhina <30803618+klepikhina@users.noreply.github.com>
2022-08-08 12:48:04 -0700

    Update books.js

--- js/books.js
+++ js/books.js
@@ -9,19 +9,19 @@ const data = {
         {
           name: "audio",
           children: [
-            { name: '"This Is Your Mind on Plants", Michael Pollan', value: "https://www.amazon.com/This-Your-Mind-Plants/dp/B08RF1K2LD" },
-            { name: '"Noise: A Flaw in Human Judgment", Daniel Kahneman', value: "https://www.amazon.com/Noise-Human-Judgment-Daniel-Kahneman/dp/0316451401" },
-            { name: '"Emperor Mollusk Versus the Sinister Brain", A. Lee Martinez', value: "https://www.amazon.com/Emperor-Mollusk-Versus-Sinister-Brain/dp/B007H3JHGG" }
+            { name: '"This Is Your Mind on Plants", Michael Pollan', value: "This-Your-Mind-Plants/dp/B08RF1K2LD" },
+            { name: '"Noise: A Flaw in Human Judgment", Daniel Kahneman', value: "Noise-Human-Judgment-Daniel-Kahneman/dp/0316451401" },
+            { name: '"Emperor Mollusk Versus the Sinister Brain", A. Lee Martinez', value: "Emperor-Mollusk-Versus-Sinister-Brain/dp/B007H3JHGG" }
           ]
         },
         {
           name: "physical",
           children: [
-            { name: '"Her Body and Other Parties", Carmen Maria Machado', value: "https://www.amazon.com/Her-Body-Other-Parties-Stories/dp/155597788X" },
-            { name: '"The Song of Achilles", Madeline Miller', value: "https://www.amazon.com/Song-Achilles-Novel-Madeline-Miller/dp/0062060627" },
-            { name: '"Man\'s 4th Best Hospital", Samuel Shem', value: "https://www.amazon.com/Mans-4th-Best-Hospital-audiobook/dp/B07ZS414F2" },
-            { name: '"Entangled Life", Merlin Sheldrake', value: "https://www.amazon.com/Entangled-Life-Worlds-Change-Futures/dp/052551032X" },
-            { name: '"The Happiness Equation", Neil Pasricha', value: "https://www.amazon.com/Happiness-Equation-Nothing-Anything-Everything/dp/0425277984" }
+            { name: '"Her Body and Other Parties", Carmen Maria Machado', value: "Her-Body-Other-Parties-Stories/dp/155597788X" },
+            { name: '"The Song of Achilles", Madeline Miller', value: "Song-Achilles-Novel-Madeline-Miller/dp/0062060627" },
+            { name: '"Man\'s 4th Best Hospital", Samuel Shem', value: "Mans-4th-Best-Hospital-audiobook/dp/B07ZS414F2" },
+            { name: '"Entangled Life", Merlin Sheldrake', value: "Entangled-Life-Worlds-Change-Futures/dp/052551032X" },
+            { name: '"The Happiness Equation", Neil Pasricha', value: "Happiness-Equation-Nothing-Anything-Everything/dp/0425277984" }
           ]
         }
       ]
@@ -32,35 +32,35 @@ const data = {
         {
           name: "audio",
           children: [
-            { name: '"The Spies of Warsaw", Alan Furst', value: "https://www.amazon.com/Spies-Warsaw-Novel-Alan-Furst/dp/0812977378" },
-            { name: '"A Conjuring of Light", V. E. Schwab', value: "https://www.amazon.com/Conjuring-Light-Novel-Shades-Magic/dp/0765387476" },
-            { name: '"A Dead Djinn in Cairo", P. Djèlí Clark', value: "https://www.amazon.com/Dead-Djinn-Cairo-Tor-Com-Original-ebook/dp/B01DJ0NALI" },
-            { name: '"The Midnight Library", Matt Haig', value: "https://www.amazon.com/Midnight-Library-Novel-Matt-Haig/dp/0525559477" },
-            { name: '"The Armies of Those I Love", Ken Liu', value: "https://www.amazon.com/Armies-Those-I-Love/dp/B08S75Z8CR" },
-            { name: '"Braving the Wilderness", Brené Brown', value: "https://www.amazon.com/Braving-Wilderness-Quest-Belonging-Courage/dp/0812995848" },
-            { name: '"Fire on the Mountain", John N Maclean', value: "https://www.amazon.com/Fire-Mountain-Story-South-Canyon/dp/0061829617" },
-            { name: '"Akata Witch", Nnedi Okorafor', value: "https://www.amazon.com/Akata-Witch-Nnedi-Okorafor/dp/0142420913" },
-            { name: '"The Color of Law", Richard Rothstein', value: "https://www.amazon.com/Color-Law-Forgotten-Government-Segregated/dp/1631494538" },
-            { name: '"Project Hail Mary", Andy Weir', value: "https://www.amazon.com/Project-Hail-Mary-Novel-Random/dp/0593395565" },
-            { name: '"Neuroscience of Everyday Life", Sam Wang', value: "https://www.amazon.com/Neuroscience-of-Everyday-Life-audiobook/dp/B07PLKYG53" },
-            { name: '"MEM", Bethany C. Morrow', value: "https://www.amazon.com/MEM-Bethany-C-Morrow/dp/1944700552" },
+            { name: '"The Spies of Warsaw", Alan Furst', value: "Spies-Warsaw-Novel-Alan-Furst/dp/0812977378" },
+            { name: '"A Conjuring of Light", V. E. Schwab', value: "Conjuring-Light-Novel-Shades-Magic/dp/0765387476" },
+            { name: '"A Dead Djinn in Cairo", P. Djèlí Clark', value: "Dead-Djinn-Cairo-Tor-Com-Original-ebook/dp/B01DJ0NALI" },
+            { name: '"The Midnight Library", Matt Haig', value: "Midnight-Library-Novel-Matt-Haig/dp/0525559477" },
+            { name: '"The Armies of Those I Love", Ken Liu', value: "Armies-Those-I-Love/dp/B08S75Z8CR" },
+            { name: '"Braving the Wilderness", Brené Brown', value: "Braving-Wilderness-Quest-Belonging-Courage/dp/0812995848" },
+            { name: '"Fire on the Mountain", John N Maclean', value: "Fire-Mountain-Story-South-Canyon/dp/0061829617" },
+            { name: '"Akata Witch", Nnedi Okorafor', value: "Akata-Witch-Nnedi-Okorafor/dp/0142420913" },
+            { name: '"The Color of Law", Richard Rothstein', value: "Color-Law-Forgotten-Government-Segregated/dp/1631494538" },
+            { name: '"Project Hail Mary", Andy Weir', value: "Project-Hail-Mary-Novel-Random/dp/0593395565" },
+            { name: '"Neuroscience of Everyday Life", Sam Wang', value: "Neuroscience-of-Everyday-Life-audiobook/dp/B07PLKYG53" },
+            { name: '"MEM", Bethany C. Morrow', value: "MEM-Bethany-C-Morrow/dp/1944700552" },
           ]
         },
         {
           name: "physical",
           children: [
-            { name: '"One Life", Megan Rapinoe', value: "https://www.amazon.com/One-Life-Megan-Rapinoe/dp/1984881167" },
-            { name: '"Circe", Madeline Miller', value: "https://www.amazon.com/Circe-Madeline-Miller/dp/0316556327" },
-            { name: '"Homegoing", Yaa Gyasi', value: "https://www.amazon.com/Homegoing-Yaa-Gyasi/dp/1101971061" },
-            { name: '"The House in the Cerulean Sea", TJ Klune', value: "https://www.amazon.com/House-Cerulean-Sea-TJ-Klune/dp/1250217288" },
-            { name: '"Whitewalling: Art, Race & Protest in 3 Acts", Aruna D\'Souza', value: "https://www.amazon.com/Whitewalling-Art-Race-Protest-Acts/dp/1943263140" },
-            { name: '"Why Women Have Better Sex Under Socialism", Kristen R. Ghodsee', value: "https://www.amazon.com/Women-Have-Better-Under-Socialism/dp/1645036367" },
-            { name: '"The Invisible Life of Addie LaRue", V. E. Schwab', value: "https://www.amazon.com/Invisible-Life-Addie-LaRue/dp/0765387565" },
-            { name: '"Partners in Crime", Agatha Christie', value: "https://www.amazon.com/Partners-Crime-Tuppence-Agatha-Christie/dp/0062074369" },
-            { name: '"Crying in H Mart", Michelle Zauner', value: "https://www.amazon.com/Crying-Mart-Memoir-Michelle-Zauner/dp/0525657746" },
-            { name: '"Eleanor Oliphant is Completely Fine", Gail Honeyman', value: "https://www.amazon.com/Eleanor-Oliphant-Completely-Fine-Novel/dp/0735220697" },
-            { name: '"The Mothers", Brit Bennett', value: "https://www.amazon.com/Mothers-Novel-Brit-Bennett/dp/039918452X" },
-            { name: '"The Golden Spruce", John Vaillant', value: "https://www.amazon.com/John-Vaillant-Golden-Madness-Paperback/dp/B01FOD91R0" }
+            { name: '"One Life", Megan Rapinoe', value: "One-Life-Megan-Rapinoe/dp/1984881167" },
+            { name: '"Circe", Madeline Miller', value: "Circe-Madeline-Miller/dp/0316556327" },
+            { name: '"Homegoing", Yaa Gyasi', value: "Homegoing-Yaa-Gyasi/dp/1101971061" },
+            { name: '"The House in the Cerulean Sea", TJ Klune', value: "House-Cerulean-Sea-TJ-Klune/dp/1250217288" },
+            { name: '"Whitewalling: Art, Race & Protest in 3 Acts", Aruna D\'Souza', value: "Whitewalling-Art-Race-Protest-Acts/dp/1943263140" },
+            { name: '"Why Women Have Better Sex Under Socialism", Kristen R. Ghodsee', value: "Women-Have-Better-Under-Socialism/dp/1645036367" },
+            { name: '"The Invisible Life of Addie LaRue", V. E. Schwab', value: "Invisible-Life-Addie-LaRue/dp/0765387565" },
+            { name: '"Partners in Crime", Agatha Christie', value: "Partners-Crime-Tuppence-Agatha-Christie/dp/0062074369" },
+            { name: '"Crying in H Mart", Michelle Zauner', value: "Crying-Mart-Memoir-Michelle-Zauner/dp/0525657746" },
+            { name: '"Eleanor Oliphant is Completely Fine", Gail Honeyman', value: "Eleanor-Oliphant-Completely-Fine-Novel/dp/0735220697" },
+            { name: '"The Mothers", Brit Bennett', value: "Mothers-Novel-Brit-Bennett/dp/039918452X" },
+            { name: '"The Golden Spruce", John Vaillant', value: "John-Vaillant-Golden-Madness-Paperback/dp/B01FOD91R0" }
           ]
         }
       ]
@@ -183,7 +183,7 @@ function Tree(data, { // data is either tabular (array of objects) or hierarchy 
 chart = Tree(data, {
   label: d => d.name,
   title: (d, n) => `${n.ancestors().reverse().map(d => d.data.name).join(".")}`, // hover text
-  link: (d, n) => `https://github.com/prefuse/Flare/${n.children ? "tree" : "blob"}/master/flare/src/${n.ancestors().reverse().map(d => d.data.name).join("/")}${n.children ? "" : ".as"}`,
+  link: (d, n) => `https://www.amazon.com/${n.children ? "tree" : "blob"}/master/flare/src/${n.ancestors().reverse().map(d => d.data.name).join("/")}${n.children ? "" : ".as"}`,
 //   width: 1152,
 //   height: 1152,
 //   margin: 300
