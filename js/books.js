9540ebbf6a59c20735e424f3edae2068e336a5ef
Ksenia Lepikhina <30803618+klepikhina@users.noreply.github.com>
2022-08-09 16:05:00 -0700

    Update books.js

--- js/books.js
+++ js/books.js
@@ -1,6 +1,6 @@
   // Set the dimensions and margins of the diagram
   var margin = {top: 10, right: 10, bottom: 10, left: 10},
-      width = 960 - margin.left - margin.right,
+      width = 1500 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
 
   // ----------------------------------------
